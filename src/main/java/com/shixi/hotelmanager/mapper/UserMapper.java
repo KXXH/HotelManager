3e92203a36e81ba05fd838344f47741e8ecbf3d1
Shujie Hu <1191315155@qq.com>
2019-06-23 19:48:32 +0800

    Revert "新项目"
    This reverts commit 5b5a069e310c826756bd44702a61ea809023f2a6.

--- src/main/java/com/shixi/hotelmanager/mapper/UserMapper.java
+++ src/main/java/com/shixi/hotelmanager/mapper/UserMapper.java
@@ -1,10 +1,5 @@
 package com.shixi.hotelmanager.mapper;
 
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.shixi.hotelmanager.entity.User;
-import org.springframework.stereotype.Service;
 
-@Service
-public interface UserMapper extends BaseMapper<User>{
-
+public class UserMapper {
 }
