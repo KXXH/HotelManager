930b6053ca18fbb16194ac09577401be3123998a
zjm <zjm97@outlook.com>
2019-06-24 18:50:25 +0800

    增加用户接口

--- src/main/java/com/shixi/hotelmanager/controller/UserController.java
+++ src/main/java/com/shixi/hotelmanager/controller/UserController.java
@@ -1,22 +1,63 @@
 package com.shixi.hotelmanager.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.shixi.hotelmanager.domain.User;
 import com.shixi.hotelmanager.mapper.UserMapper;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-@RequestMapping("/user")
+@RequestMapping("/admin/user")
 @RestController
 public class UserController {
     @Autowired
     UserMapper userMapper;
 
-    @GetMapping("/get")
+    @GetMapping("/getAll")
     public List<User> list(){
         return userMapper.selectList(null);
     }
+
+    @RequestMapping("/addUser")
+    public Map<String,String> addUser(
+            @RequestParam("username") @Length(max=25,min=5) @NotBlank String username,
+            @RequestParam("password") @Length(max=64,min=6) @NotBlank String password,
+            @RequestParam("gender") @Length(max=2) String gender,
+            @RequestParam("telephone") @Length(min=11,max=15) String telephone,
+            @RequestParam("email") @Length(max=255) @Email String email,
+            @RequestParam("id_card") @Length(min=18,max=18) @NotBlank String id_card,
+            @RequestParam("avatar") @NotBlank String avatar
+    ){
+        HashMap<String,String> m=new HashMap<>();
+        QueryWrapper<User> queryWrapper=new QueryWrapper<>();
+        queryWrapper.eq("username",username)
+                .or().eq("telephone",telephone)
+                .or().eq("id_card",id_card)
+                .or().eq("email",email);
+        if(userMapper.selectCount(queryWrapper)>0){
+            m.put("status","error");
+            m.put("msg","已存在用户");
+            return m;
+        }else{
+            User user=new User();
+            user.setUsername(username);
+            user.setPassword(password);
+            user.setAvatar(avatar);
+            user.setEmail(email);
+            user.setGender(gender);
+            user.setIdCard(id_card);
+            user.setTelephone(telephone);
+            user.setUserId(123);
+            userMapper.insert(user);
+            m.put("status","ok");
+            return m;
+        }
+    }
+    
 }
--- src/main/java/com/shixi/hotelmanager/domain/User.java
+++ src/main/java/com/shixi/hotelmanager/domain/User.java
@@ -1,5 +1,6 @@
 package com.shixi.hotelmanager.domain;
 
+import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.AllArgsConstructor;
@@ -11,7 +12,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @TableName("user")
 public class User {
-    @TableId("id")
+    @TableId(value="id",type= IdType.AUTO)
     private int id;
 
     private int userId;
