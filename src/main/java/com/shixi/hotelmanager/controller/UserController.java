8fbc5d971854567c5d28463065f30859a482acdc
Shujie Hu <1191315155@qq.com>
2019-06-27 17:56:24 +0800

    密码校验

--- src/main/java/com/shixi/hotelmanager/controller/UserController.java
+++ src/main/java/com/shixi/hotelmanager/controller/UserController.java
@@ -151,8 +151,13 @@ public class UserController {
     }
 
     @RequestMapping(value = "changePasswd")
-    public Map<String,String> changePassword(ChangePasswdDTO changePasswdDTO){
+    public Map<String,String> changePassword(@Valid ChangePasswdDTO changePasswdDTO,BindingResult result){
         Map<String,String> m = new HashMap<>();
+        if (result.hasErrors()){
+            m.put("status","error");
+            m.put("msg", String.valueOf(result.getAllErrors()));
+            return m;
+        }
         System.out.println(changePasswdDTO.getOldPassword());
         int flag = userService.changePasswd(changePasswdDTO);
         if(flag==1){
