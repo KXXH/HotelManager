a7b0b3599ae614b94aca6b5e03e121e161c064aa
hjh2333 <564004755@qq.com>
2019-07-02 11:01:35 +0800

    testvue
    酒店内房型显示

--- src/main/java/com/shixi/hotelmanager/controller/UserController.java
+++ src/main/java/com/shixi/hotelmanager/controller/UserController.java
@@ -7,6 +7,11 @@ import com.shixi.hotelmanager.exception.UserNotFoundException;
 import com.shixi.hotelmanager.mapper.UserMapper;
 import com.shixi.hotelmanager.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+<<<<<<< Updated upstream
+=======
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.ui.Model;
+>>>>>>> Stashed changes
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -133,4 +138,72 @@ public class UserController {
         m.put("msg","已删除"+result+"条!");
         return m;
     }
+<<<<<<< Updated upstream
+=======
+
+    @RequestMapping(value = "changePasswd")
+    public Map<String,String> changePassword(@Valid ChangePasswdDTO changePasswdDTO,BindingResult result){
+        Map<String,String> m = new HashMap<>();
+        if (result.hasErrors()){
+            m.put("status","error");
+            m.put("msg", String.valueOf(result.getAllErrors()));
+            return m;
+        }
+        System.out.println(changePasswdDTO.getOldPassword());
+        int flag = userService.changePasswd(changePasswdDTO);
+        if(flag==1){
+            m.put("status","ok");
+            m.put("msg","修改密码成功");
+        }
+        else if (flag == 2){
+            m.put("status","error");
+            m.put("msg","密码不一致!");
+        }
+        else if (flag == 3){
+            m.put("status","error");
+            m.put("msg","原密码不正确!");
+        }
+        return m;
+    }
+    @RequestMapping("/updateUserinfo")
+    public DefaultReturnDTO updateUser(User user) throws UserInfoDuplicateException, UserNotFoundException {
+        userService.updateUserInfo(user);
+        return new DefaultSuccessDTO();
+    }
+
+
+    @RequestMapping("/updateTelephone")
+    public DefaultReturnDTO updateTelephone(
+            @Validated({UpdateTelephoneValudation.class}) User user,
+            BindingResult result,
+            @RequestParam("code") int code,
+            HttpSession session
+            ) throws UserNotFoundException, UserInfoDuplicateException {
+        if(result.hasErrors()){
+            throw new ValidationException(result.getAllErrors().iterator().next().toString());
+        }
+        if(userService.updateTelephone(user,code,session.getId())){
+            return new DefaultSuccessDTO();
+        }else{
+            return new VerificationFailDTO();
+            //ttt
+        }
+    }
+
+
+    @RequestMapping("/forgetPassword")
+    public DefaultReturnDTO forgetPassword(@Valid ForgetPasswordDTO forgetPasswordDTO, BindingResult bindingResult,HttpSession session) throws UserNotFoundException, UserInfoDuplicateException, VerificationFailException {
+        if(bindingResult.hasErrors()){
+            throw new ValidationException(bindingResult.getAllErrors().iterator().next().toString());
+        }
+        userService.forgetPasssword(
+                forgetPasswordDTO.getTelephone(),
+                forgetPasswordDTO.getNewPassword(),
+                forgetPasswordDTO.getCode(),
+                session.getId()
+        );
+        return new DefaultSuccessDTO();
+    }
+
+>>>>>>> Stashed changes
 }
