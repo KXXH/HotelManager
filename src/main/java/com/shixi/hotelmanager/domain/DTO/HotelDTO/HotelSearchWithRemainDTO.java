c9d61636e2599f35017c24b4f40a5145154bb732
zjm <zjm97@outlook.com>
2019-07-04 14:22:15 +0800

    整合一下

--- src/main/java/com/shixi/hotelmanager/domain/DTO/HotelDTO/HotelSearchWithRemainDTO.java
+++ src/main/java/com/shixi/hotelmanager/domain/DTO/HotelDTO/HotelSearchWithRemainDTO.java
@@ -4,10 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Pattern;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class HotelSearchWithRemainDTO extends HotelSearchDTO {
-private String startDate;
-private String endDate;
+    @Pattern(regexp = "dddd-dd-dd", message="传入的日期必须是yyyy-MM-dd形式，含前导0")
+    private String startDate;
+    @Pattern(regexp = "dddd-dd-dd", message="传入的日期必须是yyyy-MM-dd形式，含前导0")
+    private String endDate;
 }
