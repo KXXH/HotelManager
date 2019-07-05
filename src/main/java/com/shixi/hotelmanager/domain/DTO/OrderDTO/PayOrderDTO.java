a4ffc6a8e93a3b7e0538875b4a85cc83c9553843
hjh2333 <564004755@qq.com>
2019-07-05 10:31:34 +0800

    room和PayOderDTO
    房间修改

--- src/main/java/com/shixi/hotelmanager/domain/DTO/OrderDTO/PayOrderDTO.java
+++ src/main/java/com/shixi/hotelmanager/domain/DTO/OrderDTO/PayOrderDTO.java
@@ -4,12 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class PayOrderDTO extends OrderDTO {
-    @NotBlank
+    @Min(value=1)
     private Long id;
 }
