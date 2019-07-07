ebc45eae6fda90e19c284b2e7c75357baf31fed8
zjm <zjm97@outlook.com>
2019-07-07 10:36:29 +0800

    修正了无法退款的问题

--- src/test/java/com/shixi/hotelmanager/HotelmanagerApplicationTests.java
+++ src/test/java/com/shixi/hotelmanager/HotelmanagerApplicationTests.java
@@ -1,16 +1,9 @@
 package com.shixi.hotelmanager;
 
-import com.shixi.hotelmanager.domain.DTO.OrderDTO.CreateOrderDTO;
-import com.shixi.hotelmanager.exception.HotelRoomInsufficientException;
-import com.shixi.hotelmanager.service.OrderService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import javax.xml.bind.ValidationException;
-import java.text.ParseException;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -22,25 +15,5 @@ public class HotelmanagerApplicationTests {
     public void contextLoads() {
     }
 
-    @Autowired
-    OrderService orderService;
-    @Test
-    public void testForCreatOrder() throws ValidationException {
-        CreateOrderDTO dto = new CreateOrderDTO();
-        dto.setDateStart("2019-07-01");
-        dto.setDateEnd("2019-07-07");
-        dto.setOrderRoomId(432);
-        dto.setRoomCount(1);
-        dto.setTelephone("13456747075");
-        dto.setPeopleCount(2);
-        dto.setPersonName("zjm");
-        try {
-            orderService.createOrder(dto);
-        } catch (HotelRoomInsufficientException e) {
-            e.printStackTrace();
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-    }
 
 }
